3f740d566150f5833d1ba8a3012fe54af971b76b
halukijl <halukijl@users.noreply.github.com>
2020-01-23 07:51:19 -0500

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+expectedformat = '%Y-%m-%d %H:%M:%S'
+
+dt = None
+a = input()
+try:
+    dt = datetime.strptime(a, expectedformat)
+
+except ValueError:
+    print('Invalid datetime')
+
+print(dt)
+
+yeari = None
+try:
+    yeari = int(year)
+except:
+    pass
+if year1 is not None:
+
+    if yeari < 1900:
+        print ("Year must be greater than 1900")
+    elif yeari > 2020:
+        print ("Year must be less than 2020"
+    else:
+        print ("Year not valid")
+
+else:
+    print ("Year "+year+" is invalid")
