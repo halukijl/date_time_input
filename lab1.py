6dc58a5cc1f1c6c4aeb23c4a4cc2410fcd05ebf1
halukijl <halukijl@users.noreply.github.com>
2020-01-23 11:02:01 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -10,21 +10,18 @@ except ValueError:
     print('Invalid datetime')
 
 print(dt)
-'''
+
 yeari = None
 try:
     yeari = int(year)
 except:
     pass
 if year1 is not None:
-
     if yeari < 1900:
         print ("Year must be greater than 1900")
     elif yeari > 2020:
         print ("Year must be less than 2020"
     else:
         print ("Year not valid")
-
 else:
     print ("Year "+year+" is invalid")
-'''
